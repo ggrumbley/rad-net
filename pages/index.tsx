fdc8818dddaf0f8acffcebf2838e01f67f1350a8
Gary Grumbley <ggrumbley@gmail.com>
2020-07-10 16:34:57 -0700

    feat(new css terminal theme)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
-
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <div>
+      <header>
+        <h1>Rad NET</h1>
+      </header>
+      <p>
+        This is a test. <mark>This should be green</mark>
+      </p>
+      <a href="#">TEST</a>
+    </div>
+  );
 }
