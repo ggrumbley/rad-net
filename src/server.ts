14500ca086142a26dcd9d0f348f3f107eb24ee7a
gary <Gary_Grumbley@intuit.com>
2021-04-12 16:58:10 -0700

    feat(server): init koa with hello world

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,26 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+
+import * as Koa from 'koa';
+import * as Router from '@koa/router';
+import * as logger from 'koa-logger';
+import * as json from 'koa-json';
+
+const app = new Koa();
+const router = new Router();
+
+app.use(json());
+app.use(logger());
+
+router.get('/', async (ctx, next) => {
+  ctx.body = { msg: 'Hello world!' };
+
+  await next();
+});
+
+// Routes
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => {
+  console.log(`Koa listening on port ${3000}`);
+});
