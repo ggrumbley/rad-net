09c069d39c370a8055af018fb570962c2e2d1e51
gary <Gary_Grumbley@intuit.com>
2021-04-15 12:45:36 -0700

    refactor: break out controllers

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,15 @@ import { createConnection } from 'typeorm';
 import express from 'express';
 import morgan from 'morgan';
 
-import authRoutes from './routes/auth';
+import { authRouter, testRouter } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'Worlds!' });
-});
+app.use('/', testRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, async () => {
   console.log(`Server running at http://localhost:${3000}`);
