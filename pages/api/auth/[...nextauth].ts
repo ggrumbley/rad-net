d95871d2d3f25075bff6488fd9bf7f0f2ce0f0a0
gary <Gary_Grumbley@intuit.com>
2021-04-27 16:58:22 -0700

    chore: add next-auth back to project

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,25 @@
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import Adapters from 'next-auth/adapters';
+import prisma from '../../../lib/prisma';
+
+const options = {
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Providers.Auth0({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      domain: process.env.AUTH0_DOMAIN,
+    }),
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+  secret: process.env.SECRET,
+};
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+
+export default authHandler;
